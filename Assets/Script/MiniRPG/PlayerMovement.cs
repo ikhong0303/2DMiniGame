4749941b041d4ad699d0bf28039547a692445f0b
githong <rhavkd33@naver.com>
2025-10-15 19:23:41 +0900

    minirpgup

---
+++ Assets/Script/MiniRPG/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody2D rb;
+
+    // Fixed Update is called 50x frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+}
